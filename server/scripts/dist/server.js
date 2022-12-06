34eebc59a154c65735376fd5ea3ebb6bd442592b
Ryan Nono <79291357+ryannono@users.noreply.github.com>
2022-12-05 19:50:46 -0500

    Server creation

---
+++ server/scripts/dist/server.js
@@ -0,0 +1,211 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const assert_1 = __importDefault(require("assert"));
+const puppeteer_1 = __importDefault(require("puppeteer"));
+const convert_array_to_csv_1 = require("convert-array-to-csv");
+const fs = __importStar(require("fs"));
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+function getBrowser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield puppeteer_1.default.launch();
+    });
+}
+function clickElement(page, selector) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield page.evaluate(passedSelector => {
+            const element = document.querySelector(passedSelector);
+            if (element) {
+                element.click();
+                return true;
+            }
+            else {
+                return false;
+            }
+        }, selector);
+    });
+}
+function selectElement(page, selector, optionIndex) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield page.evaluate((passedSelector, passedOptionIndex) => {
+            const element = document.querySelector(passedSelector);
+            if (element) {
+                element.selectedIndex = passedOptionIndex + 1;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }, selector, optionIndex);
+    });
+}
+function login(page, username, password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const loginURL = 'https://sms-sgs.ic.gc.ca/login/auth';
+        const userSelector = '#Username';
+        const passSelector = '#Password';
+        const loginBttnSelector = 'input[value="Login"]';
+        yield page.goto(loginURL);
+        (0, assert_1.default)(page.url() === loginURL);
+        yield page.type(userSelector, username);
+        yield page.type(passSelector, password);
+        const clickResponse = yield clickElement(page, loginBttnSelector);
+        if (clickResponse) {
+            yield page.waitForNavigation();
+            return true;
+        }
+        return false;
+    });
+}
+function navToTablePage(page) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield page.goto('https://sms-sgs.ic.gc.ca/product/listOwn/index?lang=en_CA');
+        const selectAccSelector = '#changeClient';
+        const submitBttnSelector = '#changeAccountButton';
+        yield selectElement(page, selectAccSelector, 1);
+        const clickResponse = yield clickElement(page, submitBttnSelector);
+        if (clickResponse) {
+            yield page.waitForNavigation();
+            return true;
+        }
+        return false;
+    });
+}
+function navToNextTablePage(page) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const nextPageBttnSelector = "a[rel = 'next']";
+        const clickResponse = yield clickElement(page, nextPageBttnSelector);
+        if (clickResponse) {
+            yield page.waitForNavigation();
+            return true;
+        }
+        return false;
+    });
+}
+function getTable(page) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let table = {
+            heading: [''],
+            body: [['']],
+            bodyLen: 0,
+        };
+        let successfullNavIndicator = true;
+        while (successfullNavIndicator) {
+            table = yield page.evaluate(table => {
+                const rows = document.querySelectorAll('tr');
+                if (!table.heading[0]) {
+                    table.heading = Array.from(rows[0].cells, headingText => headingText.innerText);
+                }
+                const rowsLen = rows.length;
+                for (let i = 1; i < rowsLen; i++) {
+                    const currentRowCellArray = Array.from(rows[i].cells, el => el.innerText);
+                    table.body[table.bodyLen] = currentRowCellArray;
+                    table.bodyLen++;
+                }
+                return table;
+            }, table);
+            successfullNavIndicator = yield navToNextTablePage(page);
+        }
+        return table;
+    });
+}
+function getDate() {
+    let dateString = new Date().toLocaleDateString('en-GB');
+    dateString =
+        dateString.substring(0, 2) +
+            '_' +
+            dateString.substring(3, 5) +
+            '_' +
+            dateString.substring(6);
+    return dateString;
+}
+function createCSVFile(writeContent, fileName, fileExtension) {
+    const dateString = getDate();
+    const fullFileName = fileName + '_' + dateString + '.' + fileExtension;
+    const writeStream = fs.createWriteStream(fullFileName, {
+        flags: 'w+',
+    });
+    writeStream.write(writeContent);
+    return writeStream;
+}
+function generateCSVString(username, password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const browser = yield getBrowser();
+        const page = yield browser.newPage();
+        yield login(page, username, password);
+        yield navToTablePage(page);
+        const table = yield getTable(page);
+        const header = table.heading;
+        const body = table.body;
+        const csvString = (0, convert_array_to_csv_1.convertArrayToCSV)(body, {
+            header,
+            separator: ',',
+        });
+        yield browser.close();
+        return csvString;
+    });
+}
+const app = (0, express_1.default)();
+const port = 3000;
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({
+    extended: true,
+}));
+app.use((0, cors_1.default)());
+app.get('/', (req, res) => {
+    res.send('Welcome to the server');
+});
+app.post('/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log(username);
+    console.log(password);
+    const csvString = yield generateCSVString(username, password);
+    const writestream = createCSVFile(csvString, 'Active_Licences_Export', 'csv');
+    const readstream = fs.createReadStream(writestream.path);
+    writestream.close;
+    const path = readstream.path;
+    res.download(path);
+    readstream.close;
+}));
+app.listen(port, () => {
+    console.log('server is running on port 3000');
+});
---
+++ server/scripts/src/server.ts
@@ -0,0 +1,286 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import assert from 'assert';
+import puppeteer, {Browser, Page} from 'puppeteer';
+import {convertArrayToCSV} from 'convert-array-to-csv';
+import * as fs from 'fs';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+// -----------------  Page Initialization Functions ------------------------//
+
+// getBrowser initialises the puppeteer browser
+async function getBrowser(): Promise<Browser> {
+  return await puppeteer.launch();
+}
+
+// -------------------  Page Navigation Helper Functions --------------------//
+
+// clickElement performs a DOM "click" on the passed selector
+// on the passed puppeteer page
+async function clickElement(page: Page, selector: string): Promise<boolean> {
+  return await page.evaluate(passedSelector => {
+    const element: HTMLElement | null = document.querySelector(passedSelector);
+    if (element) {
+      element.click();
+      return true;
+    } else {
+      return false;
+    }
+  }, selector);
+}
+
+// selectElement DOM "select's" the option at the passed index
+// on the passed select element selector on the passed puppeteer page
+async function selectElement(
+  page: Page,
+  selector: string,
+  optionIndex: number
+): Promise<boolean> {
+  return await page.evaluate(
+    (passedSelector: string, passedOptionIndex: number) => {
+      const element: HTMLSelectElement | null =
+        document.querySelector(passedSelector);
+      if (element) {
+        element.selectedIndex = passedOptionIndex + 1;
+        return true;
+      } else {
+        return false;
+      }
+    },
+    selector,
+    optionIndex
+  );
+}
+
+// ----------------------  Page Navigation Functions -----------------------//
+
+// login logs-in to the SMS using the credentials in the .env file
+async function login(
+  page: Page,
+  username: string,
+  password: string
+): Promise<boolean> {
+  // variables
+  const loginURL = 'https://sms-sgs.ic.gc.ca/login/auth';
+  const userSelector = '#Username';
+  const passSelector = '#Password';
+  const loginBttnSelector = 'input[value="Login"]';
+
+  // initialisation
+  await page.goto(loginURL);
+  assert(page.url() === loginURL);
+
+  // enter user name & password
+  await page.type(userSelector, username);
+  await page.type(passSelector, password);
+
+  // Login
+  const clickResponse = await clickElement(page, loginBttnSelector);
+  if (clickResponse) {
+    await page.waitForNavigation();
+    return true;
+  }
+  return false;
+}
+
+// navToLicencesList navigates to the first licence table page in the SMS
+async function navToTablePage(page: Page): Promise<boolean> {
+  await page.goto('https://sms-sgs.ic.gc.ca/product/listOwn/index?lang=en_CA');
+
+  const selectAccSelector = '#changeClient';
+  const submitBttnSelector = '#changeAccountButton';
+
+  // select account option
+  await selectElement(page, selectAccSelector, 1);
+
+  // navigate to list of license applications
+  const clickResponse = await clickElement(page, submitBttnSelector);
+  if (clickResponse) {
+    await page.waitForNavigation();
+    return true;
+  }
+  return false;
+}
+
+// navToNextTablePage navigates to the next table page and returns true if one exists
+// if no next page exists the funtion returns false
+async function navToNextTablePage(page: Page): Promise<boolean> {
+  const nextPageBttnSelector = "a[rel = 'next']";
+
+  const clickResponse: boolean = await clickElement(page, nextPageBttnSelector);
+  if (clickResponse) {
+    await page.waitForNavigation();
+    return true;
+  }
+  return false;
+}
+
+// ----------------------  Table Creation Function -----------------------//
+
+// getTable generates and returns a custom table object that holds'
+// the license content from all tables in the SMS
+async function getTable(
+  page: Page
+): Promise<{heading: string[]; body: string[][]; bodyLen: number}> {
+  // table structure
+  let table = {
+    heading: [''],
+    body: [['']],
+    bodyLen: 0,
+  };
+
+  // get table body content from all pages
+  let successfullNavIndicator = true;
+
+  // iterate through all pages
+  while (successfullNavIndicator) {
+    // add all table body content to our table object
+    table = await page.evaluate(table => {
+      // get rows
+      const rows: NodeListOf<HTMLTableRowElement> =
+        document.querySelectorAll('tr');
+
+      // set heading
+      if (!table.heading[0]) {
+        table.heading = Array.from(
+          rows[0].cells,
+          headingText => headingText.innerText
+        );
+      }
+
+      // get number of rows on page
+      const rowsLen: number = rows.length;
+
+      // iterate through all rows adding them to our table body
+      for (let i = 1; i < rowsLen; i++) {
+        const currentRowCellArray: string[] = Array.from(
+          rows[i].cells,
+          el => el.innerText
+        );
+        table.body[table.bodyLen] = currentRowCellArray;
+        table.bodyLen++;
+      }
+
+      // new table
+      return table;
+    }, table);
+
+    successfullNavIndicator = await navToNextTablePage(page);
+  }
+
+  return table;
+}
+
+// ------------------------  CSV Export Functions ------------------------//
+
+// getDate generates formats and returns a date to be used for version
+// control of CSV exports
+function getDate(): string {
+  let dateString: string = new Date().toLocaleDateString('en-GB');
+
+  // proper date format
+  dateString =
+    dateString.substring(0, 2) +
+    '_' +
+    dateString.substring(3, 5) +
+    '_' +
+    dateString.substring(6);
+
+  return dateString;
+}
+
+// createCSVFile write a csv file with the given content and name
+function createCSVFile(
+  writeContent: string,
+  fileName: string,
+  fileExtension: string
+) {
+  const dateString: string = getDate();
+  const fullFileName: string =
+    fileName + '_' + dateString + '.' + fileExtension;
+
+  // create writestream
+  const writeStream = fs.createWriteStream(fullFileName, {
+    flags: 'w+',
+  });
+
+  // write
+  writeStream.write(writeContent);
+
+  return writeStream;
+}
+
+// exportLicensesCSV navigates to the SMS and exports all your licence applications
+async function generateCSVString(username: string, password: string) {
+  // initialisation
+  const browser: Browser = await getBrowser();
+  const page: Page = await browser.newPage();
+
+  // login to government sms (Spectrum Management System)
+  await login(page, username, password);
+
+  // go to table repo/page
+  await navToTablePage(page);
+
+  // generate a table object from all license pages
+  const table = await getTable(page);
+  const header: string[] = table.heading;
+  const body: string[][] = table.body;
+
+  // generate csv string from table
+  const csvString: string = convertArrayToCSV(body, {
+    header,
+    separator: ',',
+  });
+
+  await browser.close();
+
+  return csvString;
+}
+
+// -------------------------------  Main --------------------------------//
+
+// initialise server params
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+
+// teaser :)
+app.get('/', (req, res) => {
+  res.send('Welcome to the server');
+});
+
+// Route that handles credentials input logic
+app.post('/login', async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  console.log(username);
+  console.log(password);
+
+  // write file
+  const csvString = await generateCSVString(username, password);
+  const writestream = createCSVFile(csvString, 'Active_Licences_Export', 'csv');
+
+  // access file path
+  const readstream = fs.createReadStream(writestream.path);
+  writestream.close;
+  const path = readstream.path as string;
+
+  // download file
+  res.download(path);
+  readstream.close;
+});
+
+app.listen(port, () => {
+  console.log('server is running on port 3000');
+});
+// exportLicensesCSV();
