02c7e8a1d18e0172a58735fab0d4b9b7694b774d
Ryan Nono <79291357+ryannono@users.noreply.github.com>
2022-12-07 01:47:44 -0500

    refactoring

---
+++ server/scripts/dist/generateCSVString.js
@@ -0,0 +1,150 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateCSVString = exports.getDate = void 0;
+const assert_1 = __importDefault(require("assert"));
+const puppeteer_1 = __importDefault(require("puppeteer"));
+const convert_array_to_csv_1 = require("convert-array-to-csv");
+function getBrowser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield puppeteer_1.default.launch();
+    });
+}
+function clickElement(page, selector) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield page.evaluate(passedSelector => {
+            const element = document.querySelector(passedSelector);
+            if (element) {
+                element.click();
+                return true;
+            }
+            else {
+                return false;
+            }
+        }, selector);
+    });
+}
+function selectElement(page, selector, optionIndex) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield page.evaluate((passedSelector, passedOptionIndex) => {
+            const element = document.querySelector(passedSelector);
+            if (element) {
+                element.selectedIndex = passedOptionIndex + 1;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }, selector, optionIndex);
+    });
+}
+function login(page, username, password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const loginURL = 'https://sms-sgs.ic.gc.ca/login/auth';
+        const userSelector = '#Username';
+        const passSelector = '#Password';
+        const loginBttnSelector = 'input[value="Login"]';
+        yield page.goto(loginURL);
+        (0, assert_1.default)(page.url() === loginURL);
+        yield page.type(userSelector, username);
+        yield page.type(passSelector, password);
+        const clickResponse = yield clickElement(page, loginBttnSelector);
+        if (clickResponse) {
+            yield page.waitForNavigation();
+            return true;
+        }
+        return false;
+    });
+}
+function navToTablePage(page) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield page.goto('https://sms-sgs.ic.gc.ca/product/listOwn/index?lang=en_CA');
+        const selectAccSelector = '#changeClient';
+        const submitBttnSelector = '#changeAccountButton';
+        yield selectElement(page, selectAccSelector, 1);
+        const clickResponse = yield clickElement(page, submitBttnSelector);
+        if (clickResponse) {
+            yield page.waitForNavigation();
+            return true;
+        }
+        return false;
+    });
+}
+function navToNextTablePage(page) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const nextPageBttnSelector = "a[rel = 'next']";
+        const clickResponse = yield clickElement(page, nextPageBttnSelector);
+        if (clickResponse) {
+            yield page.waitForNavigation();
+            return true;
+        }
+        return false;
+    });
+}
+function getTable(page) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let table = {
+            heading: [''],
+            body: [['']],
+            bodyLen: 0,
+        };
+        let successfullNavIndicator = true;
+        while (successfullNavIndicator) {
+            table = yield page.evaluate(table => {
+                const rows = document.querySelectorAll('tr');
+                if (!table.heading[0]) {
+                    table.heading = Array.from(rows[0].cells, headingText => headingText.innerText);
+                }
+                const rowsLen = rows.length;
+                for (let i = 1; i < rowsLen; i++) {
+                    const currentRowCellArray = Array.from(rows[i].cells, el => el.innerText);
+                    table.body[table.bodyLen] = currentRowCellArray;
+                    table.bodyLen++;
+                }
+                return table;
+            }, table);
+            successfullNavIndicator = yield navToNextTablePage(page);
+        }
+        return table;
+    });
+}
+function getDate() {
+    let dateString = new Date().toLocaleDateString('en-GB');
+    dateString =
+        dateString.substring(0, 2) +
+            '_' +
+            dateString.substring(3, 5) +
+            '_' +
+            dateString.substring(6);
+    return dateString;
+}
+exports.getDate = getDate;
+function generateCSVString(username, password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const browser = yield getBrowser();
+        const page = yield browser.newPage();
+        yield login(page, username, password);
+        yield navToTablePage(page);
+        const table = yield getTable(page);
+        const header = table.heading;
+        const body = table.body;
+        const csvString = (0, convert_array_to_csv_1.convertArrayToCSV)(body, {
+            header,
+            separator: ',',
+        });
+        yield browser.close();
+        return csvString;
+    });
+}
+exports.generateCSVString = generateCSVString;
--- server/scripts/dist/server.js
+++ server/scripts/dist/server.js
@@ -12,146 +12,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const assert_1 = __importDefault(require("assert"));
-const puppeteer_1 = __importDefault(require("puppeteer"));
-const convert_array_to_csv_1 = require("convert-array-to-csv");
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-function getBrowser() {
-    return __awaiter(this, void 0, void 0, function* () {
-        return yield puppeteer_1.default.launch();
-    });
-}
-function clickElement(page, selector) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return yield page.evaluate(passedSelector => {
-            const element = document.querySelector(passedSelector);
-            if (element) {
-                element.click();
-                return true;
-            }
-            else {
-                return false;
-            }
-        }, selector);
-    });
-}
-function selectElement(page, selector, optionIndex) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return yield page.evaluate((passedSelector, passedOptionIndex) => {
-            const element = document.querySelector(passedSelector);
-            if (element) {
-                element.selectedIndex = passedOptionIndex + 1;
-                return true;
-            }
-            else {
-                return false;
-            }
-        }, selector, optionIndex);
-    });
-}
-function login(page, username, password) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const loginURL = 'https://sms-sgs.ic.gc.ca/login/auth';
-        const userSelector = '#Username';
-        const passSelector = '#Password';
-        const loginBttnSelector = 'input[value="Login"]';
-        yield page.goto(loginURL);
-        (0, assert_1.default)(page.url() === loginURL);
-        yield page.type(userSelector, username);
-        yield page.type(passSelector, password);
-        const clickResponse = yield clickElement(page, loginBttnSelector);
-        if (clickResponse) {
-            yield page.waitForNavigation();
-            return true;
-        }
-        return false;
-    });
-}
-function navToTablePage(page) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield page.goto('https://sms-sgs.ic.gc.ca/product/listOwn/index?lang=en_CA');
-        const selectAccSelector = '#changeClient';
-        const submitBttnSelector = '#changeAccountButton';
-        yield selectElement(page, selectAccSelector, 1);
-        const clickResponse = yield clickElement(page, submitBttnSelector);
-        if (clickResponse) {
-            yield page.waitForNavigation();
-            return true;
-        }
-        return false;
-    });
-}
-function navToNextTablePage(page) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const nextPageBttnSelector = "a[rel = 'next']";
-        const clickResponse = yield clickElement(page, nextPageBttnSelector);
-        if (clickResponse) {
-            yield page.waitForNavigation();
-            return true;
-        }
-        return false;
-    });
-}
-function getTable(page) {
-    return __awaiter(this, void 0, void 0, function* () {
-        let table = {
-            heading: [''],
-            body: [['']],
-            bodyLen: 0,
-        };
-        let successfullNavIndicator = true;
-        while (successfullNavIndicator) {
-            table = yield page.evaluate(table => {
-                const rows = document.querySelectorAll('tr');
-                if (!table.heading[0]) {
-                    table.heading = Array.from(rows[0].cells, headingText => headingText.innerText);
-                }
-                const rowsLen = rows.length;
-                for (let i = 1; i < rowsLen; i++) {
-                    const currentRowCellArray = Array.from(rows[i].cells, el => el.innerText);
-                    table.body[table.bodyLen] = currentRowCellArray;
-                    table.bodyLen++;
-                }
-                return table;
-            }, table);
-            successfullNavIndicator = yield navToNextTablePage(page);
-        }
-        return table;
-    });
-}
-function getDate() {
-    let dateString = new Date().toLocaleDateString('en-GB');
-    dateString =
-        dateString.substring(0, 2) +
-            '_' +
-            dateString.substring(3, 5) +
-            '_' +
-            dateString.substring(6);
-    return dateString;
-}
-function generateCSVString(username, password) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const browser = yield getBrowser();
-        const page = yield browser.newPage();
-        yield login(page, username, password);
-        yield navToTablePage(page);
-        const table = yield getTable(page);
-        const header = table.heading;
-        const body = table.body;
-        const csvString = (0, convert_array_to_csv_1.convertArrayToCSV)(body, {
-            header,
-            separator: ',',
-        });
-        yield browser.close();
-        return csvString;
-    });
-}
+const generateCSVString_1 = require("./generateCSVString");
 const app = (0, express_1.default)();
 const port = 3001;
 app.use(body_parser_1.default.json());
@@ -167,8 +33,8 @@ app.post('/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () 
     const password = req.body.password;
     console.log(username);
     console.log(password);
-    const csvString = yield generateCSVString(username, password);
-    const filePath = path_1.default.join('exports', 'Active_Licences_Export_' + getDate() + '.csv');
+    const csvString = yield (0, generateCSVString_1.generateCSVString)(username, password);
+    const filePath = path_1.default.join('exports', 'Active_Licences_Export_' + (0, generateCSVString_1.getDate)() + '.csv');
     fs_1.default.writeFileSync(filePath, csvString);
     res.download(filePath, err => {
         if (err)
