10c0a92a2a1f4233ea5c6030f8dc339bf7f4170f
Ryan Nono <79291357+ryannono@users.noreply.github.com>
2022-12-11 14:54:52 -0500

    .

--- server/scripts/src/server.ts
+++ server/scripts/src/server.ts
@@ -11,7 +11,7 @@ dotenv.config;
 
 /* This is setting up the server. */
 const app = express();
-const port = Number(process.env.PORT) || 3001;
+const port = Number(process.env.PORT) || 8080;
 
 /* This is setting up the body parser and cors. */
 app.use(bodyParser.json());
