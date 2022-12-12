16a7244272536d0a9f5f08b14bf08580db5b3d88
Ryan Nono <79291357+ryannono@users.noreply.github.com>
2022-12-12 18:54:44 -0500

    continued setting up database

--- database/scripts/src/databaseScripts.ts
+++ database/scripts/src/databaseScripts.ts
@@ -1,8 +1,12 @@
 import {PrismaClient} from '@prisma/client';
 const prisma = new PrismaClient();
 
-async function newUser() {
-  const user = await prisma.user.create({data: {name: 'Ryan'}});
-  const users = await prisma.user.findMany();
-  console.log(user);
-}
+// async function newUser(email:string, password:string) {
+//   const user = await prisma.user.create({data: {name: 'Test'}});
+//   console.log('user: ' + user + ' has been created');
+// }
+
+// async function deleteUser(email:string, password:string) {
+//   const user = await prisma.user.findFirst(name: )
+//   console.log('user: ' + user + ' has been created');
+// }
