ccf0d4f46b311efca923924f0f0ad401cb1e2f52
Ryan Nono <ryan_nono@hotmail.com>
2022-12-20 03:23:36 -0500

    added blueprint

--- frontend/src/pages/dashboard-page/dashboardPage.tsx
+++ frontend/src/pages/dashboard-page/dashboardPage.tsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {userContext} from '../../globals/app-contexts';
-// eslint-disable-next-line node/no-extraneous-import
-import {Home} from '@mui/icons-material';
+import './dashboardPage.css';
+import {Icon} from '@blueprintjs/core';
 
 export default class Dashboard extends Component {
   static contextType = userContext;
@@ -9,8 +9,17 @@ export default class Dashboard extends Component {
 
   render() {
     return (
-      <div className="sideBar">
-        <Home />
+      <div>
+        <div className="sideBarContainer">
+          <div className="verticalIconButtonContainer">
+            <Icon icon="home" size={20} />
+            <Icon icon="pie-chart" size={20} />
+          </div>
+          <div>
+            <Icon icon="cog" size={20} />
+          </div>
+        </div>
+        <div className="dashboardWrapper"></div>
       </div>
     );
   }
