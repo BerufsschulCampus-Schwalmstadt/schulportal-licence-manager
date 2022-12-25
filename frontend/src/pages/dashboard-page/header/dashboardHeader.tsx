345e52cea15f7c9ddc876952061f109afab758f2
Ryan Nono <ryan_nono@hotmail.com>
2022-12-24 19:37:40 -0500

    header complete

--- frontend/src/pages/dashboard-page/header/dashboardHeader.tsx
+++ frontend/src/pages/dashboard-page/header/dashboardHeader.tsx
@@ -1,5 +1,5 @@
-import Dropdown from '../../../components/dropdown/dropdown';
-import IconButton from '../../../components/icon-button/iconButton';
+import Dropdown from '../../../components/molecules/dropdown/dropdown';
+import IconButton from '../../../components/molecules/icon-button/iconButton';
 import './dashboardHeader.css';
 import React, {Component} from 'react';
 
@@ -7,30 +7,34 @@ export default class DashboardHeader extends Component {
   render() {
     return (
       <div className="dashboardHeaderWrapper">
-        <h4>Licence account</h4>
-        <Dropdown
-          id={'licenceAccountDropdown'}
-          filled={'white'}
-          outlined={{colour: '#B3B1EC'}}
-          options={['619888398273', '619218398273', '619768398273']}
-        />
-        <p>Last synced 5 days ago</p>
-        <IconButton
-          clickHandler={() => {}}
-          iconName="refresh"
-          outlined={{colour: '#B3B1EC'}}
-        />
-        <IconButton
-          clickHandler={() => {}}
-          iconName="arrow-down"
-          buttonText={{text: 'Download as CSV', textPosition: 'back'}}
-        />
-        <IconButton
-          clickHandler={() => {}}
-          iconName="more"
-          filled="default"
-          size="lg"
-        />
+        <div className="dashboardHeaderleftContainer">
+          <h5>Licence account</h5>
+          <Dropdown
+            id={'licenceAccountDropdown'}
+            filled={'white'}
+            outlined={{colour: '#B3B1EC'}}
+            options={['619888398273', '619218398273', '619768398273']}
+          />
+        </div>
+        <div className="dashboardHeaderRightContainer">
+          <h4>Last synced 5 days ago</h4>
+          <IconButton
+            clickHandler={() => {}}
+            iconName="refresh"
+            outlined={{colour: '#B3B1EC'}}
+          />
+          <IconButton
+            clickHandler={() => {}}
+            iconName="arrow-down"
+            buttonText={{text: 'Download as CSV', textPosition: 'back'}}
+          />
+          <IconButton
+            clickHandler={() => {}}
+            iconName="more"
+            filled="default"
+            size="lg"
+          />
+        </div>
       </div>
     );
   }
